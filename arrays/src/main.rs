6a1a50d2b4d7df004cd60dba9b051280e78dbf6d
tehtko <moderndsnsam@gmail.com>
2022-12-31 01:10:00 -0500

    Arrays and iteration

---
+++ arrays/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut array: [usize; 50] = [0; 50];
+
+    for i in 0..array.len()         // exclusive of the top number
+    {
+        array[i] = i * i;
+    }
+
+    for i in 0..=array.len() - 1    //inclusive of the top number
+    {                           
+        array[i] = i * (i + 1);
+    }
+
+    println!("{:#?}", array);
+}
