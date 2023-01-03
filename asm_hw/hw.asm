1fa2c0ba77f0578902e84c7d8968a95be5c1d570
tehtko <moderndsnsam@gmail.com>
2023-01-02 20:46:43 -0500

    Exit syscall

--- asm_hw/hw.asm
+++ asm_hw/hw.asm
@@ -5,15 +5,16 @@
 .section .text
 
 _start:
-    # mov %eax, 0x01  # exit syscall
-    # mov %ebx, 0x0   # error code 
-    # int 0x80        # invoke system call
 
-    mov %eax, 0x04 # write syscal
-    mov %ebx, 0x01 # stdout
-    lea %ecx, [message] # load effective address 
-    mov %edx, 13
+    mov %eax, 0x04          # write syscal
+    mov %ebx, 0x01          # stdout address
+    lea %ecx, [message]     # load effective address 
+    mov %edx, 13            # size of message
     int 0x80
+
+    mov %eax, 0x01          # exit syscall
+    mov %ebx, 0x0           # error code 
+    int 0x80                # invoke system call
 
 # write Hello World to output
 
