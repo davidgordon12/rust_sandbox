6212c0894f14ed2ff4333627119dcb2215f4519e
David Gordon <44374315+tehtko@users.noreply.github.com>
2023-01-02 21:40:16 -0500

    Update hw.asm

--- asm_hw/hw.asm
+++ asm_hw/hw.asm
@@ -6,15 +6,15 @@
 
 _start:
 
-    mov %eax, 0x04          # write syscal
+    mov %eax, 0x04          # write systemcall
     mov %ebx, 0x01          # stdout address
     lea %ecx, [message]     # load effective address 
     mov %edx, 13            # size of message
     int 0x80
 
-    mov %eax, 0x01          # exit syscall
+    mov %eax, 0x01          # exit systemcall
     mov %ebx, 0x0           # error code 
-    int 0x80                # invoke system call
+    int 0x80                # invoke systemcall
 
 # write Hello World to output
 
